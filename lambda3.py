dd1681bd18bd60aaf5a86f240fb09810cb333453
Ubuntu <ubuntu@ip-172-31-63-155.ec2.internal>
2024-11-28 04:23:53 +0000

    Correr el crawler cada 5 mins

--- lambda3.py
+++ lambda3.py
@@ -1,34 +1,35 @@
 import boto3
-import json
+import logging
 
-# Inicializar cliente de Glue
-glue_client = boto3.client('glue')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def start_crawler(crawler_name: str):
+    """Función que inicia el Crawler de Glue."""
+    glue_client = boto3.client('glue', region_name='us-east-1')
+    
+    try:
+        # Iniciar el Crawler de Glue
+        response = glue_client.start_crawler(Name=crawler_name)
+        logger.info(f"Crawler {crawler_name} iniciado con éxito.")
+    except Exception as e:
+        logger.error(f"Error al iniciar el Crawler: {str(e)}")
 
 def lambda_run(event, context):
-    """
-    Lambda para ejecutar el Crawler de Glue.
-    """
-    print("Se ejecuto 0")
-    try:
-        # Definir el nombre del Crawler
-        crawler_name = 'headlinesfinal'  # Cambia esto por el nombre de tu Crawler
-        print("Se ejecuto 1")
+    """Función principal que será ejecutada por Lambda."""
+    # Inicializa el nombre del Crawler directamente en el código
+    crawler_name = 'headlinesfinal'  # Nombre del Crawler de Glue
 
-        # Ejecutar el Crawler
-        response = glue_client.start_crawler(Name=crawler_name)
-        print("Se ejecuto 2")
-        
-        # Mostrar respuesta para depuración
-        print(f"Respuesta del Crawler: {json.dumps(response, indent=2)}")
+    if not crawler_name:
+        logger.error("El nombre del Crawler no está configurado correctamente.")
+        return
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps(f"Crawler '{crawler_name}' ejecutado con éxito.")
-        }
-
-    except Exception as e:
-        print(f"Error al ejecutar el Crawler: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f"Error al ejecutar el Crawler: {str(e)}")
-        }
+    logger.debug(f"Iniciando el Crawler {crawler_name}...")
+    
+    # Llamar a la función que inicia el Crawler
+    start_crawler(crawler_name)
+    
+    return {
+        "statusCode": 200,
+        "body": f"Crawler {crawler_name} iniciado correctamente"
+    }
